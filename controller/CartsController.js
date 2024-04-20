992a9f93fafbf608ab8df505f51ce2a6296628ed
yunho <Yunho7058@github.com>
2024-04-20 23:31:38 +0900

    API

---
+++ controller/CartsController.js
@@ -0,0 +1,50 @@
+//문윤호
+const conn = require('../mariadb');
+const { StatusCodes } = require('http-status-codes');
+let dotenv = require('dotenv');
+dotenv.config();
+
+const getAllItem = (req, res) => {
+  const { userId, selected } = req.body;
+  conn.query(
+    `SELECT cartItems.id, book_id, title, summary, quantity, price 
+    FROM cartItems LEFT JOIN books 
+    ON cartItems.book_id = books.id;
+    WHERE user_id=?  AND id IN (?)`,
+    [userId, selected],
+    (err, results) => {
+      if (err) {
+        res.status(StatusCodes.BAD_REQUEST).end();
+        //throw err;
+      }
+      return res.status(StatusCodes.OK).json(results);
+    }
+  );
+};
+const addItem = (req, res) => {
+  const { bookId, quntity, userId } = req.body;
+  conn.query(
+    `INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?);`,
+    [bookId, quntity, userId],
+    (err, results) => {
+      if (err) {
+        res.status(StatusCodes.BAD_REQUEST).end();
+        //throw err;
+      }
+      return res.status(StatusCodes.OK).json(results);
+    }
+  );
+};
+const removeItem = (req, res) => {
+  const { userId } = req.body;
+  let sql = `DELETE FROM cartitems WHERE user_id=?`;
+  conn.query(sql, userId, (err, resulst) => {
+    if (err) {
+      throw err;
+    }
+    // 만약 좋아요 테이블에 내가 좋아요 누름 테이블 있을경우
+    res.status(StatusCodes.OK).json(resulst);
+  });
+};
+
+module.exports = { getAllItem, addItem, removeItem };
--- routes/likes.js
+++ routes/likes.js
@@ -1,14 +1,12 @@
+//문윤호
 const express = require('express');
-//const conn = require('../mariadb');
-//const { body, validationResult, param } = require('express-validator');
 const router = express.Router();
-//let jwt = require('jsonwebtoken');
 let dotenv = require('dotenv');
 router.use(express.json()); //json 형태로 사용
 dotenv.config();
 const { likeAdd, likeRemove } = require('../controller/LikesController');
 
-router.post('/', likeAdd);
+router.post('/:id', likeAdd);
 
 router.delete('/:id', likeRemove);
 
